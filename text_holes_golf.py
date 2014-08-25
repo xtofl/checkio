f78988c501b66452e8bbae66b72865152220fc37
xtofl <xtofl@github.com>
2014-08-25 18:47:56 +0200

    lambda iso def

--- text_holes_golf.py
+++ text_holes_golf.py
@@ -3,7 +3,4 @@ from itertools import starmap
 
 r1 = r"\S\S\S\S\s\S\S\S\S"
 
-def golf(t):
-    return sum(
-        [len(filter(lambda c: re.match(r1, t[r-1][c-1:c+2] + t[r][c-1:c+2] + t[r+1][c-1:c+2]),
-                        xrange(1, len(t[r])))) for r in xrange(1, len(t)-1)])
+golf = lambda t: sum([len(filter(lambda c: re.match(r1, t[r-1][c-1:c+2] + t[r][c-1:c+2] + t[r+1][c-1:c+2]), xrange(1, len(t[r])))) for r in xrange(1, len(t)-1)])
