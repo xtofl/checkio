bc2ae693655cc4cf50603398243dbd324f9a0143
xtofl <xtofl@github.com>
2014-08-25 18:45:31 +0200

    functional style

--- text_holes_golf.py
+++ text_holes_golf.py
@@ -1,13 +1,12 @@
 import re
 from itertools import starmap
 
+r1 = r"\S\S\S\S\s\S\S\S\S"
+
 def golf(t):
     n = 0
-    for row in xrange(1, len(t)-1):
-        ls = t[row-1:row+2]
-        for c in xrange(1, len(ls[1])):
-            r = r"\S"*3
-            s = r"\S\s\S"
-            n += all(starmap(lambda l, r: re.match(r, l[c-1:c+2]),
-                        zip(ls, [r, s, r])))
+
+    for r in xrange(1, len(t)-1):
+        n += len(filter(lambda c: re.match(r1, t[r-1][c-1:c+2] + t[r][c-1:c+2] + t[r+1][c-1:c+2]),
+                        xrange(1, len(t[r]))))
     return n
