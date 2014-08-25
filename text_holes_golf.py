352058a607439f553060f753b25038f923ec8fb3
xtofl <xtofl@github.com>
2014-08-25 18:46:58 +0200

    sum iso accumulator

--- text_holes_golf.py
+++ text_holes_golf.py
@@ -4,9 +4,6 @@ from itertools import starmap
 r1 = r"\S\S\S\S\s\S\S\S\S"
 
 def golf(t):
-    n = 0
-
-    for r in xrange(1, len(t)-1):
-        n += len(filter(lambda c: re.match(r1, t[r-1][c-1:c+2] + t[r][c-1:c+2] + t[r+1][c-1:c+2]),
-                        xrange(1, len(t[r]))))
-    return n
+    return sum(
+        [len(filter(lambda c: re.match(r1, t[r-1][c-1:c+2] + t[r][c-1:c+2] + t[r+1][c-1:c+2]),
+                        xrange(1, len(t[r])))) for r in xrange(1, len(t)-1)])
