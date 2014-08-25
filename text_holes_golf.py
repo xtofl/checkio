a044dc27b54f903d7ec4add2743f84e65dc78035
xtofl <xtofl@github.com>
2014-08-25 16:48:40 +0200

    variable names one character

--- text_holes_golf.py
+++ text_holes_golf.py
@@ -1,13 +1,13 @@
 import re
 from itertools import starmap
 
-def golf(text):
+def golf(t):
     n = 0
-    for row in xrange(1, len(text)-1):
-        lines = text[row-1:row+2]
-        for col in xrange(1, len(lines[1])):
-            r1 = r"\S"*3
-            r2 = r"\S\s\S"
-            n += all(starmap(lambda line, r: re.match(r, line[col-1:col+2]),
-                        zip(lines, [r1, r2, r1])))
+    for row in xrange(1, len(t)-1):
+        ls = t[row-1:row+2]
+        for c in xrange(1, len(ls[1])):
+            r = r"\S"*3
+            s = r"\S\s\S"
+            n += all(starmap(lambda l, r: re.match(r, l[c-1:c+2]),
+                        zip(ls, [r, s, r])))
     return n
