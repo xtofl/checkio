07d71950b63bdbe120c2b7e8051ff139d5f7d987
xtofl <xtofl@github.com>
2014-08-25 16:40:11 +0200

    remove unneeded test

--- text_holes_golf.py
+++ text_holes_golf.py
@@ -1,13 +1,10 @@
 import re
+from itertools import starmap
 
 def row_matches(lines, col):
-    if all(map(lambda r: len(r) > col+1, lines)):
-        splice = lambda line: line[col-1:col+2]
-        spliced = map(splice, lines)
-        return all([
-            re.match(r"\S\S\S", spliced[0]),
-            re.match(r"\S\s\S", spliced[1]),
-            re.match(r"\S\S\S", spliced[2])])
+    match = lambda line, r: re.match(r, line[col-1:col+2])
+    spliced = starmap(match, zip(lines, [r"\S\S\S", r"\S\s\S", r"\S\S\S"]))
+    return all(spliced)
 
 def golf(text):
     def iterate(text):
@@ -15,6 +12,5 @@ def golf(text):
             lines = text[row-1:row+2]
             for col in xrange(1, len(lines[row])):
                 if row_matches(lines, col):
-                    print row, col
                     yield row, col
     return len(list(iterate(text)))
