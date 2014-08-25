4b1bc26f7c9442c3a03be42072c982da19877566
xtofl <xtofl@github.com>
2014-08-25 18:48:36 +0200

    inlined + remove whitespace

--- text_holes_golf.py
+++ text_holes_golf.py
@@ -1,6 +1,2 @@
 import re
-from itertools import starmap
-
-r1 = r"\S\S\S\S\s\S\S\S\S"
-
-golf = lambda t: sum([len(filter(lambda c: re.match(r1, t[r-1][c-1:c+2] + t[r][c-1:c+2] + t[r+1][c-1:c+2]), xrange(1, len(t[r])))) for r in xrange(1, len(t)-1)])
+golf = lambda t: sum([len(filter(lambda c: re.match(r"\S\S\S\S\s\S\S\S\S", t[r-1][c-1:c+2] + t[r][c-1:c+2] + t[r+1][c-1:c+2]), xrange(1, len(t[r])))) for r in xrange(1, len(t)-1)])
