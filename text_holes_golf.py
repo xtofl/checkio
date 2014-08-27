4efdde0f3d9fc360b929554775a395d229bf4662
xtofl <xtofl@github.com>
2014-08-27 08:08:26 +0200

    constant regex

--- text_holes_golf.py
+++ text_holes_golf.py
@@ -1,2 +1,2 @@
 import re
-golf=lambda t:len(re.findall(r"(?=({s}.{{17}}\S\s\S.{{17}}{s}))".format(s="\S"*3),"".join(map(lambda l:l+" "*(20-len(l)),t))))
+golf=lambda t:len(re.findall(r"(?=(\S\S\S.{27}\S\s\S.{27}\S\S\S))","".join(map(lambda l:l.ljust(30, " "),t))))
