2c2566a58e5e5a51e98fac91284fd8ddecda02f2
xtofl <xtofl@github.com>
2014-08-27 07:28:11 +0200

    densify

--- text_holes_golf.py
+++ text_holes_golf.py
@@ -1,5 +1,4 @@
 import re
-
 def golf(t):
-    m = max(map(len, t))
-    return len(re.findall(r"(?=(\S\S\S.{{{n}}}\S\s\S.{{{n}}}\S\S\S))".format(n=m-3), "".join(map(lambda l: l + " "*(m - len(l)), t))))
+    m=max(map(len, t))
+    return len(re.findall(r"(?=({s}.{{{n}}}\S\s\S.{{{n}}}{s}))".format(n=m-3,s="\S\S\S"),"".join(map(lambda l:l+" "*(m-len(l)),t))))
