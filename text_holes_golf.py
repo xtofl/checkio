2e9009d77de2f7f634831e8ab8543b3c8afef52d
xtofl <xtofl@github.com>
2014-08-27 07:24:36 +0200

    regex: followed-by to allow overlap

--- text_holes_golf.py
+++ text_holes_golf.py
@@ -2,5 +2,4 @@ import re
 
 def golf(t):
     m = max(map(len, t))
-    j = "".join(map(lambda l: l + " "*(m - len(l)), t))
-    return len(filter(lambda t: re.match(r"\S\S\S.{{{n}}}\S\s\S.{{{n}}}\S\S\S".format(n=m-3), t), [j[c:] for c in xrange(len(j))]))
+    return len(re.findall(r"(?=(\S\S\S.{{{n}}}\S\s\S.{{{n}}}\S\S\S))".format(n=m-3), "".join(map(lambda l: l + " "*(m - len(l)), t))))
