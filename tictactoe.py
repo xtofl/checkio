023d440b690203998ce420df5282b4c47bc70b45
xtofl <xtofl@github.com>
2014-09-06 18:17:09 +0200

    [refac] add framework for n-d slicing

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,46 @@
 
+def valid(grid, coord):
+    if not coord: return True
+    return len(grid) > coord[0] and valid(grid[coord[0]], coord[1:])
+
+def resolve(grid, coord):
+    if len(coord) == 1:
+        return grid[coord[0]]
+    else:
+        return resolve(grid[coord[0]], coord[1:])
+
+def slice(grid, start, step):
+    current = start
+    while valid(grid, current):
+        yield resolve(grid, current)
+        current = [a + b for a, b in zip(current, step)]
+
+
+from unittest import TestCase
+
+class STestCase(TestCase):
+    def assertSEqual(self, s1, s2):
+        return super().assertEqual(list(s1), list(s2))
+
+class TestSlice2D(STestCase):
+
+    def test_Valid(self):
+        self.assertTrue(valid([], []))
+        self.assertTrue(valid([1], (0)))
+        self.assertFalse(valid([1], (1,)))
+        self.assertTrue(valid([[], [1, 2]], (1, 1)))
+        self.assertFalse(valid([[], [1, 2]], (0, 1)))
+
+    def test_One(self):
+        self.assertSEqual([1, 2], slice([1, 2], (0,), (1,)))
+
+        grid = [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]]
+        self.assertSEqual([1, 2, 3], slice(grid, (0, 0), (0, 1)))
+        self.assertSEqual([2, 3], slice(grid, (0, 1), (0, 1)))
+        self.assertSEqual([1, 5, 9], slice(grid, (0, 0), (1, 1)))
+        self.assertSEqual([3, 5, 7], slice(grid, (0, 2), (1, -1)))
 
 column = lambda grid, col: [row[col] for row in grid]
 vertical = lambda grid, side, col: [side]*3 == list(column(grid, col))
@@ -22,10 +64,7 @@ def checkio(grid):
 
 from unittest import TestCase
 
-class Test(TestCase):
-
-    def assertSEqual(self, s1, s2):
-        return self.assertEqual(list(s1), list(s2))
+class Test(STestCase):
 
     def test_Column(self):
         self.assertSEqual([1, 2, 3], column([
