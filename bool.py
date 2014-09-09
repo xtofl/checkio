88b843e2c3ed5c326af6db80f1d63f6ccdddf9d1
Kristoffel Pirard <kristoffel.pirard@tomra.com>
2014-09-09 11:52:22 +0200

    [refac] expressed boolean operators as a dsl from the assignment doc

--- bool.py
+++ bool.py
@@ -1,18 +1,29 @@
+import re
+def boolean(x, y, op):
+    definition="""
+     x | y | conjunction | disjunction | implication | exclusive | equivalence |
+    --------------------------------------
+     0 | 0 |  0  |  0  |  1  |  0  |  1  |
+     1 | 0 |  0  |  1  |  0  |  1  |  0  |
+     0 | 1 |  0  |  1  |  1  |  1  |  0  |
+     1 | 1 |  1  |  1  |  1  |  0  |  1  |
+    --------------------------------------
+    """.splitlines()
+    operators = [x_.strip() for x_ in re.split("\|", definition[1])[2:] if x_]
 
-def boolean(val1, val2, op):
-    return {
-        "conjunction": lambda a, b: a and b,
-        "disjunction": lambda a, b: a or b,
-        "implication": lambda a, b: not a or b,
-        "exclusive": lambda a, b: (a or b) and not (a and b),
-        "equivalence": lambda a, b: a == b
-    }[op](val1, val2)
+    table = {op: {} for op in operators}
+    for entry in definition[3:-2]:
+        values = [int(v.strip()) for v in re.split("\|", entry) if v]
+        a, b = values[0:2]
+        for operator, value in zip(operators, values[2:]):
+            table[operator][(a, b)] = value
 
+    return table[op][(x, y)]
 
 from unittest import TestCase
 
 
 class TestBool(TestCase):
     def test_One(self):
-        self.assertEqual(True, boolean(True, True, "conjunction"))
-        self.assertEqual(False, boolean(False, True, "conjunction"))
+        self.assertEqual(1, boolean(1, 1, "conjunction"))
+        self.assertEqual(0, boolean(0, 1, "conjunction"))
