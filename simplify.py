0651852e77df967460258cbc68641996acbfc089
xtofl <xtofl@github.com>
2014-09-13 19:50:14 +0200

    [impl] basic simplify for each class

--- simplify.py
+++ simplify.py
@@ -1,3 +1,4 @@
+from itertools import product
 from operator import mul
 
 __mission__ = """http://www.checkio.org/mission/simplification/share/cd052a5416a6e8b459e74b63451b9c30/"""
@@ -6,7 +7,8 @@ from unittest import TestCase
 
 
 class Expr:
-    pass
+    def __repr__(self):
+        return self.abs_fmt()
 
 
 class Constant(Expr):
@@ -19,6 +21,9 @@ class Constant(Expr):
     def abs_fmt(self):
         return str(abs(self.value))
 
+    def simplify(self):
+        return [self]
+
     def pattern(self):
         return "c"
 
@@ -29,6 +34,9 @@ class Power(Expr):
 
     def abs_fmt(self):
         return "x**{}".format(self.exponent)
+
+    def simplify(self):
+        return self
 
     def pattern(self):
         return "^"
@@ -45,6 +53,9 @@ class Term(Expr):
     def abs_fmt(self):
         return "{}*{}".format(self.factor.abs_fmt(), self.exponent.abs_fmt())
 
+    def simplify(self):
+        return [self]
+
     def pattern(self):
         return "t"
 
@@ -57,14 +68,12 @@ class Product(Expr):
         return "*"
 
     def simplify(self):
-        if all([f.pattern() == "t" for f in self.factors]):
-            return Term(reduce(mul, [f.factor for f in self.factors]))
+        if all(lambda t: (t)==t.simplify() for t in self.factors):
+            return [self]
+        terms0 = self.factors[0].simplify()
+        terms1 = self.factors[1].simplify()
+        return (Product(t0, t1).simplify() for t0, t1 in product(terms0, terms1))
 
-class Reductions:
-
-    @staticmethod
-    def distribute(terms, factor):
-        return [(t, factor) for t in terms]
 
 class Sum(Expr):
     def __init__(self, *terms):
@@ -75,6 +84,9 @@ class Sum(Expr):
                "".join([
                    ('-' if t.negative() else '+') + t.abs_fmt()
                    for t in self.terms[1:]])
+
+    def simplify(self):
+        return reduce(lambda lst, t: lst+(t), (t.simplify() for t in self.terms))
 
     def pattern(self):
         return "".join([t.pattern() for t in self.terms])
@@ -87,6 +99,12 @@ def parse(expr):
 def simplify(expr):
     return str(parse(expr))
 
+
+class Reductions:
+
+    @staticmethod
+    def distribute(factor, terms):
+        return tuple((t, factor) for t in terms)
 
 class TestSimplify(TestCase):
 
@@ -114,8 +132,12 @@ class TestSimplify(TestCase):
     def test_Simplification(self):
         term1 = Term(Constant(8), Power(5))
         term2 = Term(Constant(-2), Power(2))
-        product = Product(term1, term2)
-        self.assertEqual("t", product.simplify().pattern())
+        s = Sum(term1, term2)
+        factor = Constant(5)
+        product = Product(Constant(5), s) # 5 * (8x^5 - 2x^2) = 40x^5 -10x^2
+        #self.assertEqual(((factor, term1), (factor, term2)), Reductions.distribute(factor, [term1, term2]))
+        self.assertEqual((Term(Constant(40), Power(5)), Term(Constant(-10), Power(2))),
+                         product.simplify())
 
     def _test_Given(self):
         self.assertEqual("x**2-1", simplify("(x-1)*(x+1)"))
