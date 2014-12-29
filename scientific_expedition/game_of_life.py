7cf2a7ee042ccb0b138d30633bc388fdf75f5309
xtofl <xtofl@github.com>
2014-12-29 18:38:38 +0100

    [impl] count_neighbor_matrix as intermediate

--- scientific_expedition/game_of_life.py
+++ scientific_expedition/game_of_life.py
@@ -7,14 +7,14 @@ def alive(current, live_neighbors):
     else:
         return live_neighbors == 3
 
+neighbors = ((-1, -1), (-1, 0), (-1, 1),
+             ( 0, -1),          ( 0, 1),
+             ( 1, -1), ( 1, 0), ( 1, 1))
 
 def count_live_around(state, row, col):
-    neighbors = ((-1, -1), (-1, 0), (-1, 1),
-                 ( 0, -1),          ( 0, 1),
-                 ( 1, -1), ( 1, 0), ( 1, 1))
     count = 0
     for r, c in ((row + dr, col + dc) for dr, dc in neighbors):
-        if 0 <= r < len(state) and 0 <= col < len(state[0]):
+        if 0 <= r < len(state) and 0 <= c < len(state[0]):
             therow = state[r]
             thecell = therow[c]
             count += state[r][c]
@@ -26,6 +26,19 @@ def next_row(state, r):
 
 def next_state(state):
     return tuple(tuple(next_row(state, r)) for r in range(len(state)))
+
+def neighbor_row(state, r):
+    yield from (count_live_around(state, r, c) for c in range(len(state[r])))
+
+def neighbor_matrix(state):
+    return tuple(tuple(neighbor_row(state, r)) for r in range(len(state)))
+
+def count_to_live(state, count):
+    return tuple(
+        tuple(int(alive(current, cnt))
+              for current, cnt in zip(rstate, rcount))
+        for (rstate, rcount) in zip(state, count))
+
 
 from unittest import TestCase
 
@@ -41,6 +54,40 @@ class TestGOL(TestCase):
         )
         self.assertEqual(count_live_around(m, 0, 0), 0)
         self.assertEqual(count_live_around(m, 0, 2), 1)
+
+    def test_nbmatrix(self):
+        m = (
+            (0, 0, 0, 0, 0),
+            (0, 0, 0, 1, 0),
+            (0, 0, 1, 1, 0),
+            (0, 0, 0, 0, 1)
+        )
+        self.assertEqual(neighbor_matrix(m), (
+            (0, 0, 1, 1, 1),
+            (0, 1, 3, 2, 2),
+            (0, 1, 2, 3, 3),
+            (0, 1, 2, 3, 1)
+        ))
+
+    def test_count_to_live(self):
+        m = (
+            (0, 0, 0, 0, 0),
+            (0, 0, 0, 1, 0),
+            (0, 0, 1, 1, 0),
+            (0, 0, 0, 0, 1)
+        )
+        c = (
+            (0, 0, 1, 1, 1),
+            (0, 1, 3, 2, 2),
+            (0, 1, 2, 3, 3),
+            (0, 1, 2, 3, 1)
+        )
+        self.assertEqual(count_to_live(m, c), (
+            (0, 0, 0, 0, 0),
+            (0, 0, 1, 1, 0),
+            (0, 0, 1, 1, 1),
+            (0, 0, 0, 1, 0)
+        ))
 
     def test_next_state(self):
         m = (
