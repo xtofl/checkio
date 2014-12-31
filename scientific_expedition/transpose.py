617b59b58d8275f785143064c0b2755f30de2b4e
xtofl <xtofl@github.com>
2014-12-31 14:08:13 +0100

    [refac] zip to the rescue

--- scientific_expedition/transpose.py
+++ scientific_expedition/transpose.py
@@ -1,13 +1,12 @@
 
 def transpose(rows):
-    if not rows: return []
-    return [[row[i] for row in rows] for i in range(len(rows[0]))]
+    return tuple(zip(*rows))
 
 from unittest import TestCase
 
 
 class TestTranspose(TestCase):
     def test_One(self):
-        self.assertEqual(transpose([]), [])
-        self.assertEqual(transpose([[1]]), [[1]])
-        self.assertEqual(transpose([[1, 2]]), [[1], [2]])
+        self.assertEqual(transpose([]), tuple())
+        self.assertEqual(transpose([[1]]), ((1,),))
+        self.assertEqual(transpose([[1, 2]]), ((1,), (2,)))
