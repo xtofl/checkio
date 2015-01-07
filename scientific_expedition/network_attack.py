248b602095e84768d4aff050ddeb7ec95a829ec5
xtofl <xtofl@github.com>
2015-01-07 15:35:21 +0100

    implementation with clock as actor

--- scientific_expedition/network_attack.py
+++ scientific_expedition/network_attack.py
@@ -1,92 +1,151 @@
+from collections import defaultdict
+from functools import partial
+
 def capture(grid_definition, maxlevel=1000):
+
     pcs = [PC(row, i) for i, row in enumerate(grid_definition)]
+    for pc in pcs:
+        pc.update_connections(pcs)
 
-    number_of_ticks = 0
-    virus = Virus(pcs)
-    pcs[0].installed.add(virus.spread)
-    virus.spread(pcs[0])
-    while not all(virus in pc.installed for pc in pcs):
-        for pc in pcs:
-            pc.tick()
-        number_of_ticks += 1
-        if number_of_ticks > maxlevel: raise AssertionError("exceeded maximum number of ticks")
+    clock = Ticker()
+    virus = Virus()
+    pcs[0].infect(clock.time())
+    virus.spread(pcs[0], clock)
 
-    return number_of_ticks
+    while any(not pc.infected(clock.time()) for pc in pcs):
+        clock.tick()
+
+    return max(pc.infected_time for pc in pcs)
 
 
 class PC:
     def __init__(self, row, i):
-        self.__connections = [j for j, v in enumerate(row) if j != i and v]
-        self.__security_level = row[i]
-        self.connections = lambda: self.__connections
-        self.security_level = lambda: self.__security_level
-        self.state = self.clean
-        self.installed = set()
+        self.connected_indices = [j for j, v in enumerate(row) if j != i and v]
+        self.security_level = lambda: row[i]
+        self.infected_time = None
 
-    def tick(self):
-        self.state()
+    def infected(self, t):
+        if self.infected_time:
+            return self.infected_time <= t
+        else:
+            return False
 
-    def install(self, program):
-        if program in self.installed: pass
-        if self.state != self.clean: pass
-        counter = self.security_level()
-        def countdown():
-            nonlocal counter
-            counter -= 1
-            if counter == 0:
-                program(self)
-                self.installed.add(program)
-                self.state = self.infected
+    def update_connections(self, pcs):
+        self.__connections = [pcs[i] for i in self.connected_indices]
 
-        self.state = countdown
+    def connections(self):
+        return self.__connections
 
-    def clean(self):
-        pass
+    def start_infection(self, virus, clock):
+        infection_done = clock.time() + self.security_level()
+        if self.infect(infection_done):
+            clock.schedule(infection_done, partial(virus.spread, self, clock))
 
-    def infected(self):
-        pass
+    def infect(self, time):
+        if not self.infected_time:
+            self.infected_time = time
+            return True
+        else:
+            return False
 
 
 class Virus:
-    def __init__(self, pcs):
-        self.pcs = pcs
+    def spread(self, origin, clock):
+        for target in origin.connections():
+            target.start_infection(self, clock)
 
-    def spread(self, pc):
-        for c in pc.connections():
-            self.pcs[c].install(self.spread)
+
+class Ticker:
+    def __init__(self, t=0):
+        self.q = defaultdict()
+        self.__time = t
+
+    def time(self):
+        return self.__time
+
+    def schedule(self, time, task):
+        tasks = self.q.get(time, [])
+        tasks.append(task)
+        self.q[time] = tasks
+
+    def tick(self):
+        for t in self.q.get(self.__time, []):
+            t()
+        self.__time += 1
+
+
 
 from unittest import TestCase
 from mockito import mock, verify, when
 from mockito.mocking import mock
+from mockito.matchers import any as matchany
 
 class TestCapture(TestCase):
 
-    def test_PC_initialization(self):
-        self.assertEqual([0, 2, 3], PC([1, 8, 1, 1, 0, 0], 1).connections())
-        self.assertEqual(8, PC([1, 8, 1, 1, 0, 0], 1).security_level())
 
-    def test_after_n_ticks_virus_spreads(self):
-        pc = PC([1, 3, 1, 1, 0, 0], 1)
-        virus = mock()
-        pc.install(virus.execute)
-        pc.tick()
-        pc.tick()
-        verify(virus, 0).execute(pc)
-        pc.tick()
-        verify(virus).execute(pc)
+    def test_clock_executes_when_time_is_due(self):
+        m = mock()
+        c = Ticker(9)
+        c.schedule(10, m.exec)
+        c.schedule(10, m.exec)
+        c.tick()
+        verify(m, 0).exec()
+        c.tick()
+        verify(m, 2).exec()
+
+    def test_spreading_schedules_after_due_time(self):
+        clock = mock()
+        pc = mock()
+        connections = [mock(), mock()]
+        for target in connections:
+            when(target).connections().thenReturn([])
+            when(target).security_level().thenReturn(1)
+            when(target).infect(6).thenReturn(True)
+        when(pc).connections().thenReturn(connections)
+        when(clock).time().thenReturn(5)
+
+        v = Virus()
+        v.spread(pc, clock)
+
+        verify(clock, 2).schedule(6, matchany())
+
+    def test_PC_initialization(self):
+        pc = PC([1, 8, 1, 1, 0, 0], 1)
+        pcs = [mock(), mock(), mock(), mock(), mock(), mock()]
+        pc.update_connections(pcs)
+        self.assertEqual([pcs[0], pcs[2], pcs[3]], pc.connections())
+        self.assertEqual(8, pc.security_level())
+
+    def test_pc_infection_takes_time(self):
+        pc = PC([1, 8, 1, 1, 0, 0], 1)
+        pc.infect(10)
+        self.assertEqual(10, pc.infected_time)
+        self.assertFalse(pc.infected(9))
+        self.assertTrue(pc.infected(10))
+        self.assertTrue(pc.infected(11))
 
     def test_virus_spreads_to_connections(self):
         pc = mock()
-        pcs = [pc, mock(), mock()]
-        when(pc).connections().thenReturn([1, 2])
-        v = Virus(pcs)
-        v.spread(pc)
+        clock = mock()
+        pcs = [mock()]
+        for pc in pcs:
+            when(pc).infect(any()).thenReturn(True)
+            when(pc).security_level().thenReturn(6)
+        when(pc).connections().thenReturn(pcs)
+        v = Virus()
+        when(clock).time().thenReturn(5)
+        v.spread(pc, clock)
+        verify(clock).schedule(11, matchany())
 
     def test_Given(self):
         self.assertEqual(capture(
             [[0, 1, 1],
              [1, 2, 1],
              [1, 1, 2]]), 2)
+        self.assertEqual(capture(
+            [[0, 1, 1],
+             [1, 3, 1],
+             [1, 1, 2]]), 3)
         self.assertEqual(capture(
             [[0, 1, 0, 1, 0, 1],
              [1, 8, 1, 0, 0, 0],
